0ca0dd27f56eccdb2b1490ddb37d08c6333b3bda
FlazoCS <sian_chia@mymail.sutd.edu.sg>
2024-01-04 18:12:40 +0800

    Sian: 4/1/2024 Update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'capstone-vn';
+var cacheName = 'placeholder-name';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
