6afa2a8c50507291fe764c176d8781665e9c4abd
FlazoCS <sian_chia@mymail.sutd.edu.sg>
2024-01-19 17:24:17 +0800

    Sian: Scenario 4 Update
    Scenario 4 added.
    Title change to "Quiet, Can or Not?"
    Main Menu Box moved.
    New Portraits Added.
    Notepad added to Scenario 1.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'placeholder-name';
+var cacheName = 'quiet-can-or-not-';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
